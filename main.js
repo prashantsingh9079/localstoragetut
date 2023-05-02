facf2e7d9788ce0d1a587b87aae326bfedcff207
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-05-02 16:19:25 +0530

    task10

--- main.js
+++ main.js
@@ -9,9 +9,15 @@ function onsubmit(e){
     // document.querySelector('.container').style.background = 'red';
     // console.log(name.value);
     // console.log(mail.value);
-    localStorage.setItem("username",name.value);
-    localStorage.setItem("mail",mail.value)
-    console.log(localStorage.getItem("username"))
+    // localStorage.setItem("username",name.value);
+    // localStorage.setItem("mail",mail.value)
+    // console.log(localStorage.getItem("username"))
+
+    let data_obj ={name_stored:name.value, email_stored:mail.value};
+    let data_obj_sy = JSON.stringify(data_obj);
+    localStorage.setItem("data",data_obj_sy); 
+    let vpp = JSON.parse(data_obj_sy)
+    console.log(vpp)
 }
 
 
